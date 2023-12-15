608b03be0cf480dbfb6a92a1474abf1fa8fc16a5
rizkimadya <rizkimadya88@gmail.com>
2023-12-15 17:44:37 +0800

    crud livewire post

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//posts index
+Route::get('/', App\Livewire\Posts\Index::class)->name('posts.index');
+
+//posts create
+Route::get('/create', App\Livewire\Posts\Create::class)->name('posts.create');
+
+//posts edit
+Route::get('/edit/{id}', App\Livewire\Posts\Edit::class)->name('posts.edit');
